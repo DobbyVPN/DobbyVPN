dc093a4c86a601ae90204803c88b1d40b111f60a
Brudlord <brudloord@yandex.ru>
2025-04-30 23:53:24 +0300

    Make copy for logs

--- ck-android/CkClient/app/src/jvmMain/kotlin/com/dobby/feature/logging/CopyLogsInteractorImpl.kt
+++ ck-android/CkClient/app/src/jvmMain/kotlin/com/dobby/feature/logging/CopyLogsInteractorImpl.kt
@@ -1,9 +1,15 @@
 package com.dobby.feature.logging
 
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import com.dobby.feature.logging.domain.CopyLogsInteractor
 
-class CopyLogsInteractorImpl() : CopyLogsInteractor {
+class CopyLogsInteractorImpl : CopyLogsInteractor {
 
     override fun copy(logs: List<String>) {
+        val joinedLogs = logs.joinToString("\n")
+        val stringSelection = StringSelection(joinedLogs)
+        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        clipboard.setContents(stringSelection, null)
     }
 }
