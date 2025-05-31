ad6f79851f4d307f771a3b36a2fc8ebd6ee27980
Шевченко Будимир Максимилианович <75212610+BrudLord@users.noreply.github.com>
2025-05-31 21:50:55 +0300

    Update cloak.go

--- go_client/desktop_exports/cloak.go
+++ go_client/desktop_exports/cloak.go
@@ -1,12 +1,18 @@
 package main
 
+import "C"
 import (
 	"go_client/cloak"
 )
 
 //export StartCloakClient
-func StartCloakClient(localHost, localPort, config string, udp bool) {
-	cloak.StartCloakClient(localHost, localPort, config, udp)
+func StartCloakClient(localHost *C.char, localPort *C.char, config *C.char, udp bool) {
+	cloak.StartCloakClient(
+		C.GoString(localHost),
+		C.GoString(localPort),
+		C.GoString(config),
+		bool(udp),
+	)
 }
 
 //export StopCloakClient
