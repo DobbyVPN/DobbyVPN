5a8e5d8e24528019d3cf4651570200151c2bdfb4
Andrey Aksenov <aksenovandrey4@gmail.com>
2025-06-07 01:43:16 +0300

    C-shared mobile client

--- go_client/main.go
+++ go_client/main.go
@@ -1,26 +1,35 @@
 package main
 
+/*
+#include <stdlib.h>
+#include <string.h>
+*/
 import "C"
 import (
 	"go_client/kotlin_exports"
 	"unsafe"
 )
 
+// хранит текущее устройство
 var dev *kotlin_exports.OutlineDevice
 
 //export NewOutlineDevice
 func NewOutlineDevice(config *C.char) {
 	goConfig := C.GoString(config)
-	_dev, err := kotlin_exports.NewOutlineDevice(goConfig)
+	d, err := kotlin_exports.NewOutlineDevice(goConfig)
 	if err != nil {
+		// можно логировать ошибку, но возвращаем просто nil
+		dev = nil
 		return
 	}
-	dev = _dev
-	return
+	dev = d
 }
 
 //export Write
 func Write(buf *C.char, length C.int) C.int {
+	if dev == nil {
+		return -1
+	}
 	data := C.GoBytes(unsafe.Pointer(buf), length)
 	n, err := dev.Write(data)
 	if err != nil {
@@ -31,18 +40,24 @@ func Write(buf *C.char, length C.int) C.int {
 
 //export Read
 func Read(buf *C.char, maxLen C.int) C.int {
+	if dev == nil {
+		return -1
+	}
 	data, err := dev.Read()
 	if err != nil {
 		return -1
 	}
-
+	// сколько реально скопируем
 	copyLen := len(data)
 	if copyLen > int(maxLen) {
 		copyLen = int(maxLen)
 	}
-	C.memcpy(unsafe.Pointer(buf),
+	// memcpy из <string.h>
+	C.memcpy(
+		unsafe.Pointer(buf),
 		unsafe.Pointer(&data[0]),
-		C.size_t(copyLen))
+		C.size_t(copyLen),
+	)
 	return C.int(copyLen)
 }
 
