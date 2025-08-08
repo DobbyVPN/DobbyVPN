37205cbf7ce66243a84f5c561f093d0cff064b9d
Brud <brudloord@yandex.ru>
2025-08-08 18:01:22 +0300

    try gen ios lib

--- go_client/ios_exports/cloak.go
+++ go_client/ios_exports/cloak.go
@@ -4,7 +4,7 @@ import (
 	"go_client/cloak"
 )
 
-func StartCloakClient(localHost  *byte, localPort  *byte, config  *byte, udp bool) {
+func StartCloakClient(localHost  *string, localPort  *string, config  *string, udp bool) {
 	cloak.StartCloakClient(localHost, localPort, config, udp)
 }
 
