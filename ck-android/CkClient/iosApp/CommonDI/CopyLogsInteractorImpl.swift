eae8e2dabe0098c65df8a6f30a36a4ac69921f88
Brud <brudloord@yandex.ru>
2025-09-01 02:36:54 +0300

    Add publish to App Store

--- ck-android/CkClient/iosApp/CommonDI/CopyLogsInteractorImpl.swift
+++ ck-android/CkClient/iosApp/CommonDI/CopyLogsInteractorImpl.swift
@@ -6,16 +6,29 @@ class CopyLogsInteractorImpl: CopyLogsInteractor {
     
     func doCopy(logs: [String]) {
         let logText = logs.joined(separator: "\n")
-        let pasteboard = UIPasteboard.general
-
-        if #available(iOS 14.0, *) {
-            let provider = NSItemProvider(object: logText as NSString)
-            provider.suggestedName = "logs.txt"
-            pasteboard.setItemProviders([provider], localOnly: true, expirationDate: nil)
-        } else {
-            pasteboard.string = logText
+        
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        guard let documentsDirectory = urls.first else {
+            print("Не удалось найти Documents")
+            return
         }
-
-        print("Copied logs to iOS clipboard as text file")
+        
+        // Формат даты: 2025-09-01_02-35-10
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd_HH-mm-ss"
+        let dateString = formatter.string(from: Date())
+        
+        // Имя файла с датой
+        let fileName = "DobbyVPN_logs_\(dateString).txt"
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        
+        do {
+            try logText.write(to: fileURL, atomically: true, encoding: .utf8)
+            print("Логи сохранены в файл: \(fileURL.path)")
+        } catch {
+            print("Ошибка сохранения логов: \(error)")
+        }
     }
 }
