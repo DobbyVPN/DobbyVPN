e20028dffdffc3e55fd8d194893a08ceea3240f7
Brud <brudloord@yandex.ru>
2025-09-02 03:07:26 +0300

    Add ping

--- ck-android/CkClient/iosApp/iosApp/iOSApp.swift
+++ ck-android/CkClient/iosApp/iosApp/iOSApp.swift
@@ -9,20 +9,6 @@ import Combine
 struct iOSApp: App {
     init() {
         StartDIKt.startDI(nativeModules: [NativeModuleHolder.shared]) {_ in }
-        iOSApp.writeRouteTableToLog()
-        
-        DispatchQueue.global(qos: .background).async {
-            var howManyTimes = 6
-            while howManyTimes > 0 {
-                iOSApp.writeRouteTableToLog()
-                Thread.sleep(forTimeInterval: 5.0)
-                howManyTimes -= 1
-            }
-        }
-    }
-    
-    static func writeRouteTableToLog() {
-        NativeModuleHolder.logsRepository.writeLog(log: RouteTableManager.formatRouteTable().split(separator: "\n").joined(separator: "|||"))
     }
     
     var body: some Scene {
