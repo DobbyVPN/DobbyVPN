ac9a65599eb8e78f5bbb74a4c84f72057899c600
Brudlord <brudloord@yandex.ru>
2025-10-04 01:16:21 +0300

    Add first version of MacOS and Android sudo start

--- kmp_client/app/src/jvmMain/kotlin/Main.kt
+++ kmp_client/app/src/jvmMain/kotlin/Main.kt
@@ -1,4 +1,3 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.dobby.di.startDI
@@ -8,12 +7,73 @@ import com.dobby.ui.theme.DesktopClientTheme
 import com.sun.jna.Platform
 import org.koin.mp.KoinPlatform
 import java.io.File
+import java.io.IOException
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 
+fun ensureAdminPrivilegesMacOS() {
+    if (!isRunningAsRoot()) {
+        try {
+            val jarPath = File(
+                object {}.javaClass.protectionDomain.codeSource.location.toURI()
+            ).absolutePath
+
+            val command = arrayOf(
+                "osascript", "-e",
+                "do shell script \"java -jar '$jarPath'\" with administrator privileges"
+            )
+
+            Runtime.getRuntime().exec(command)
+            System.exit(0)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+fun ensureAdminPrivilegesLinux() {
+    if (!isRunningAsRoot()) {
+        try {
+            val jarPath = File(
+                object {}.javaClass.protectionDomain.codeSource.location.toURI()
+            ).absolutePath
+
+            val command = arrayOf(
+                "pkexec", "java", "-jar", jarPath
+            )
+
+            val process = Runtime.getRuntime().exec(command)
+            if (process.isAlive) {
+                System.exit(0)
+            } else {
+                println("Can't run with admin privileges")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+
+fun isRunningAsRoot(): Boolean {
+    return try {
+        val process = ProcessBuilder("id", "-u").start()
+        val output = process.inputStream.bufferedReader().readText().trim()
+        output == "0"
+    } catch (e: IOException) {
+        false
+    }
+}
+
 fun main() = application {
+    if (Platform.isMac()) {
+        ensureAdminPrivilegesMacOS()
+    }
+    if (Platform.isLinux()) {
+        ensureAdminPrivilegesLinux()
+    }
     startDI(listOf(jvmMainModule, jvmVpnModule)){}
-    // Получение текущего пути к jar-файлу
+    // Get path to the current jar-file
     val encodedPath = this::class.java.protectionDomain.codeSource.location.path
     val decodedPath = URLDecoder.decode(encodedPath, StandardCharsets.UTF_8.name())
     val appDir = File(decodedPath).parentFile.absolutePath
