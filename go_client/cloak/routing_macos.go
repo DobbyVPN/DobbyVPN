1ea0c0bb87930fca2948c995330a9dd3bb8e355b
Brudlord <brudloord@yandex.ru>
2025-10-25 01:34:42 +0300

    Add osascript to macos

--- go_client/cloak/routing_macos.go
+++ go_client/cloak/routing_macos.go
@@ -1,4 +1,4 @@
-//go:build darwin
+//go:build darwin && !(android || ios)
 // +build darwin
 
 package cloak
--- go_client/cloak/routing_windows.go
+++ go_client/cloak/routing_windows.go
@@ -1,4 +1,5 @@
-//go:build windows
+//go:build windows && !(android || ios)
+// +build windows
 
 package cloak
 
