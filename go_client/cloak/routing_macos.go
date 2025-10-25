9868b968404db05a3d276c7f8c95d0b518257ebe
Brudlord <brudloord@yandex.ru>
2025-10-25 03:03:39 +0300

    Move all routing to routing_macos.go

--- go_client/cloak/routing_macos.go
+++ go_client/cloak/routing_macos.go
@@ -12,38 +12,38 @@ import (
 )
 
 func StartRoutingCloak(proxyIP string) error {
-	gatewayIP, err := gateway.DiscoverGateway()
-	if err != nil {
-		return fmt.Errorf("failed to discover gateway: %w", err)
-	}
-
-	commands := []string{
-		fmt.Sprintf("route add -net %s/32 %s", proxyIP, gatewayIP.String()),
-	}
-
-	if _, err := routing.ExecuteAsAdmin(commands); err != nil {
-		log.Warnf("failed to add specific route: %v", err)
-		return err
-	}
+// 	gatewayIP, err := gateway.DiscoverGateway()
+// 	if err != nil {
+// 		return fmt.Errorf("failed to discover gateway: %w", err)
+// 	}
+//
+// 	commands := []string{
+// 		fmt.Sprintf("route add -net %s/32 %s", proxyIP, gatewayIP.String()),
+// 	}
+//
+// 	if _, err := routing.ExecuteAsAdmin(commands); err != nil {
+// 		log.Warnf("failed to add specific route: %v", err)
+// 		return err
+// 	}
 
 	log.Infof("Successfully added specific route to %s via %s", proxyIP, gatewayIP)
 	return nil
 }
 
 func StopRoutingCloak(proxyIP string) error {
-	gatewayIP, err := gateway.DiscoverGateway()
-	if err != nil {
-		return fmt.Errorf("failed to discover gateway: %w", err)
-	}
-
-	commands := []string{
-		fmt.Sprintf("route delete -net %s/32 %s", proxyIP, gatewayIP.String()),
-	}
-
-	if _, err := routing.ExecuteAsAdmin(commands); err != nil {
-		log.Warnf("failed to remove specific route: %v", err)
-		return err
-	}
+// 	gatewayIP, err := gateway.DiscoverGateway()
+// 	if err != nil {
+// 		return fmt.Errorf("failed to discover gateway: %w", err)
+// 	}
+//
+// 	commands := []string{
+// 		fmt.Sprintf("route delete -net %s/32 %s", proxyIP, gatewayIP.String()),
+// 	}
+//
+// 	if _, err := routing.ExecuteAsAdmin(commands); err != nil {
+// 		log.Warnf("failed to remove specific route: %v", err)
+// 		return err
+// 	}
 
 	log.Infof("Successfully removed specific route to %s", proxyIP)
 	return nil
