f4850a49dececbce5a8f2ff69db6d17cac2765fa
annapiatkova <anutochka366@gmail.com>
2025-12-12 00:07:04 +0300

    ios implementation

--- kmp_client/iosApp/CommonDI/AuthenticationManagerImpl.swift
+++ kmp_client/iosApp/CommonDI/AuthenticationManagerImpl.swift
@@ -1,14 +1,31 @@
 import app
+import LocalAuthentication
 
 class AuthenticationManagerImpl : AuthenticationManager {
+    private var context = LAContext()
+
     func isAuthenticationAvailable() -> Bool {
-        return false
+        var error: NSError?
+        return self.context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error)
     }
 
     func authenticate(
-    onAuthSuccess: () -> Void,
-    onAuthFailure: () -> Void
-     ) {
-        onAuthSuccess()
+        onAuthSuccess: () -> Void,
+        onAuthFailure: () -> Void
+    ) {
+        if (!isAuthenticationAvailable()) {
+            onAuthSuccess()
+            return
+        }
+        self.context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Biometric login") {
+            success, authenticatedError in
+        	DispatchQueue.main.async {
+        		if success {
+        			onAuthSuccess()
+        		} else{
+        			onAuthFailure()
+        		}
+            }
+        }
     }
 }
