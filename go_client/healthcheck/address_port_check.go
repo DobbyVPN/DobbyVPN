654a43d4606dc890b28ba3a3b470748254a46632
Brudlord <brudloord@yandex.ru>
2026-01-13 23:35:20 +0300

    Rollback status marking 2

--- go_client/healthcheck/address_port_check.go
+++ go_client/healthcheck/address_port_check.go
@@ -11,20 +11,33 @@ func CheckServerAlive(address string, port int) error {
 	target := fmt.Sprintf("%s:%d", address, port)
 
 	d := net.Dialer{
-		Timeout: 1000 * time.Millisecond,
+		Timeout: 1 * time.Second,
 	}
 
 	var lastErr error
+
 	for i := 0; i < 3; i++ {
 		conn, err := d.Dial("tcp", target)
-		if err == nil {
-			conn.Close()
-			return nil
+		if err != nil {
+			lastErr = err
+			if strings.Contains(err.Error(), "refused") {
+				return err
+			}
+			continue
 		}
-		lastErr = err
-		if strings.Contains(err.Error(), "refused") {
-			return err
+
+		defer conn.Close()
+
+		_ = conn.SetDeadline(time.Now().Add(1 * time.Second))
+
+		_, err = conn.Write([]byte{0x00})
+		if err != nil {
+			lastErr = err
+			continue
 		}
+
+		return nil
 	}
+
 	return lastErr
 }
