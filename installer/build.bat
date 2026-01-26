232eda675ab3604be720af523cc630bed23d6b12
Alebik0 <belash.aleksandr777@gmail.com>
2026-01-26 19:55:24 +0300

    Fixed Windows installer

--- installer/build.bat
+++ installer/build.bat
@@ -1,20 +1,27 @@
 @echo off
 
-set DOBBYVPN_VERSION="0.0.27"
+set DOBBYVPN_VERSION=%APP_MAJOR_VERSION%.%APP_MINOR_VERSION%.%APP_MAINTENANCE_VERSION%
+echo [+] Building DobbyVPN v%DOBBYVPN_VERSION% MSI installers
 
 :checkdeps
 	echo [+] Checking wix
 	wix --version || goto :error
-	echo [+] Downloading wintun
-	curl -#fLo wintun.zip https://www.wintun.net/builds/wintun-0.14.1.zip || goto :error
-	tar -xvzf wintun.zip || goto :error
+	
+	if exist "wintun\" echo [+] Wintun installed
+	if not exist "wintun\" call :wintun
+
 	echo [+] Checking dobby-vpn
-	if not exist dobby-vpn\ goto :error
+	if exist "dobby-vpn\" (
+		echo [+] Application folder exist
+	) else (
+		echo [+] Application folder not exist
+		goto :error
+	)
 
 :build
 	call :msi amd64 x64 || goto :error
-	@REM call :msi x86 x86 || goto :error
-	@REM call :msi arm64 arm64 || goto :error
+	call :msi x86 x86 || goto :error
+	call :msi arm64 arm64 || goto :error
 
 :success
 	echo [+] Success.
@@ -26,6 +33,15 @@ set DOBBYVPN_VERSION="0.0.27"
 
 	echo [+] Compiling %1
 	wix build -src .\Package.wxs -src .\Folders.wxs -src .\AppComponents.wxs -b .\ -d "DOBBYVPN_PLATFORM=%1" -d "DOBBYVPN_VERSION=%DOBBYVPN_VERSION%" -arch %2 -o bin/%1/dobby-vpn-%DOBBYVPN_VERSION%.msi || goto :error
+	goto :eof
+
+:wintun
+	echo [+] Downloading wintun
+	curl -#fLo wintun.zip https://www.wintun.net/builds/wintun-0.14.1.zip || goto :error
+	echo [+] Unzip wintun
+	tar -xvzf wintun.zip || goto :error
+	echo [+] Clear wintun.zip
+	del wintun.zip
 	goto :eof
 
 :error
