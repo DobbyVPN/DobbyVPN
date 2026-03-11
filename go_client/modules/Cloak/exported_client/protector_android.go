3dbd5515039ef5da8377a1d7e9c36a0c63cf65c9
Brudlord <brudloord@yandex.ru>
2026-03-12 01:38:40 +0300

    Fix android cloak

--- go_client/modules/Cloak/exported_client/protector_android.go
+++ go_client/modules/Cloak/exported_client/protector_android.go
@@ -3,121 +3,22 @@
 
 package exported_client
 
-// Stolen from https://github.com/shadowsocks/overture/blob/shadowsocks/core/utils/utils_android.go
-
 /*
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <sys/uio.h>
-
-#define ANCIL_FD_BUFFER(n)                                                     \
-  struct {                                                                     \
-    struct cmsghdr h;                                                          \
-    int fd[n];                                                                 \
-  }
-
-int ancil_send_fds_with_buffer(int sock, const int *fds, unsigned n_fds,
-                               void *buffer) {
-  struct msghdr msghdr;
-  char nothing = '!';
-  struct iovec nothing_ptr;
-  struct cmsghdr *cmsg;
-  int i;
-
-  nothing_ptr.iov_base = &nothing;
-  nothing_ptr.iov_len = 1;
-  msghdr.msg_name = NULL;
-  msghdr.msg_namelen = 0;
-  msghdr.msg_iov = &nothing_ptr;
-  msghdr.msg_iovlen = 1;
-  msghdr.msg_flags = 0;
-  msghdr.msg_control = buffer;
-  msghdr.msg_controllen = sizeof(struct cmsghdr) + sizeof(int) * n_fds;
-  cmsg = CMSG_FIRSTHDR(&msghdr);
-  cmsg->cmsg_len = msghdr.msg_controllen;
-  cmsg->cmsg_level = SOL_SOCKET;
-  cmsg->cmsg_type = SCM_RIGHTS;
-  for (i = 0; i < n_fds; i++)
-    ((int *)CMSG_DATA(cmsg))[i] = fds[i];
-  return (sendmsg(sock, &msghdr, 0) >= 0 ? 0 : -1);
-}
-
-int ancil_send_fd(int sock, int fd) {
-  ANCIL_FD_BUFFER(1) buffer;
-
-  return (ancil_send_fds_with_buffer(sock, &fd, 1, &buffer));
-}
-
-void set_timeout(int sock) {
-  struct timeval tv;
-  tv.tv_sec = 3;
-  tv.tv_usec = 0;
-  setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv,
-             sizeof(struct timeval));
-  setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&tv,
-             sizeof(struct timeval));
-}
+extern int go_protect_socket(int fd); // Импортируем функцию из C/JNI слоя
 */
 import "C"
-
 import (
-	"syscall"
-
 	"go_client/log"
+	"syscall"
 )
 
-// In Android, once an app starts the VpnService, all outgoing traffic are routed by the system
-// to the VPN app. In our case, the VPN app is ss-local. Our outgoing traffic to ck-server
-// will be routed back to ss-local which creates an infinite loop.
-//
-// The Android system provides an API VpnService.protect(int socketFD)
-// This tells the system to bypass the socket around the VPN.
 func protector(network string, address string, c syscall.RawConn) error {
-	log.Infof("Using Android VPN mode.")
-	fn := func(s uintptr) {
-		fd := int(s)
-		// Use Linux abstract namespace to avoid filesystem permissions/paths on Android.
-		// Must match the Android-side LocalServerSocket name.
-		path := "\x00protect_path"
-
-		socket, err := syscall.Socket(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
-		if err != nil {
-			log.Infof("Socket() failed: %v", err)
-			return
+	return c.Control(func(fd uintptr) {
+		res := C.go_protect_socket(C.int(fd))
+		if res != 1 {
+			log.Infof("Protect failed: go_protect_socket(fd=%d) returned %d for %s %s", fd, res, network, address)
+		} else {
+			log.Infof("Protect success: fd=%d", fd)
 		}
-
-		defer syscall.Close(socket)
-
-		C.set_timeout(C.int(socket))
-
-		err = syscall.Connect(socket, &syscall.SockaddrUnix{Name: path})
-		if err != nil {
-			log.Infof("Connect() failed: %v", err)
-			return
-		}
-
-		C.ancil_send_fd(C.int(socket), C.int(fd))
-
-		dummy := []byte{1}
-		n, err := syscall.Read(socket, dummy)
-		if err != nil {
-			log.Infof("Read() failed: %v", err)
-			return
-		}
-		if n != 1 {
-			log.Infof("Failed to protect fd: %d", fd)
-			return
-		}
-	}
-
-	if err := c.Control(fn); err != nil {
-		return err
-	}
-
-	return nil
+	})
 }
