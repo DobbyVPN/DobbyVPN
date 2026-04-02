87538e7d15b7f6db767db2a7bba0963e8e93657d
Brudlord <brudloord@yandex.ru>
2026-04-03 01:13:00 +0300

    Clear code

--- go_client/outline/internal/app_macos.go
+++ go_client/outline/internal/app_macos.go
@@ -1,5 +1,5 @@
-//go:build darwin
-// +build darwin
+//go:build darwin && !(android || ios)
+// +build darwin,!android,!ios
 
 package internal
 
--- go_client/outline/internal/app_windows.go
+++ go_client/outline/internal/app_windows.go
@@ -1,5 +1,5 @@
-//go:build windows
-// +build windows
+//go:build windows && !(android || ios)
+// +build windows,!android,!ios
 
 package internal
 
