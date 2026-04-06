d96424c1e37e7c7a4a41f8e48442315405f4e120
Alebik0 <belash.aleksandr777@gmail.com>
2026-04-06 16:41:15 +0300

    AmneziaWG on desktops

--- kmp_module/app/src/commonMain/kotlin/com/dobby/feature/main/domain/TomlConfigs.kt
+++ kmp_module/app/src/commonMain/kotlin/com/dobby/feature/main/domain/TomlConfigs.kt
@@ -3,6 +3,46 @@ package com.dobby.feature.main.domain
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class AmneziaWGConfig(
+    val Interface: AmneziaWGInterfaceConfig,
+    val Peer: List<AmneziaWGPeerConfig>,
+)
+
+@Serializable
+data class AmneziaWGInterfaceConfig(
+    val PrivateKey: String,
+    val PublicKey: String? = null,
+    val Address: String,
+    val DNS: String? = null,
+    val MTU: UInt? = null,
+    val Jc: UInt? = null,
+    val Jmin: UInt? = null,
+    val Jmax: UInt? = null,
+    val S1: UInt? = null,
+    val S2: UInt? = null,
+    val S3: UInt? = null,
+    val S4: UInt? = null,
+    val H1: UInt? = null,
+    val H2: UInt? = null,
+    val H3: UInt? = null,
+    val H4: UInt? = null,
+    val I1: String? = null,
+    val I2: String? = null,
+    val I3: String? = null,
+    val I4: String? = null,
+    val I5: String? = null,
+)
+
+@Serializable
+data class AmneziaWGPeerConfig(
+    val PublicKey: String,
+    val PresharedKey: String? = null,
+    val Endpoint: String,
+    val AllowedIPs: String,
+    val PersistentKeepalive: Int? = null,
+)
+
+@Serializable
 data class OutlineConfig(
     val Description: String? = null,
     val Server: String? = null,
@@ -56,5 +96,6 @@ data class ExcludeIPsConfig(
 data class TomlConfigs(
     val Description: String? = null,
     val Outline: OutlineConfig? = null,
+    val AmneziaWG: AmneziaWGConfig? = null,
     val ExcludeIPs: ExcludeIPsConfig? = null
 )
