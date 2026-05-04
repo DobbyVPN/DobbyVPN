e3938171d40317293fa589618a43460c64f25abc
Alebik0 <belash.aleksandr777@gmail.com>
2026-05-04 22:46:06 +0300

    Fixed IOS v3

---
+++ go_module/run.sh
@@ -0,0 +1,18 @@
+export ANDROID_SDK_ROOT=/home/nalek0/Android/Sdk
+export ANDROID_NDK_HOME=$ANDROID_SDK_ROOT/ndk/27.0.12077973
+export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+export DEBUG_PREFIX_FLAGS="-fdebug-prefix-map=$ANDROID_SDK_ROOT=/android-sdk -fdebug-prefix-map=$ANDROID_NDK_HOME=/android-ndk"
+export CGO_CFLAGS="$DEBUG_PREFIX_FLAGS ${CGO_CFLAGS:-}"
+export CGO_LDFLAGS="$DEBUG_PREFIX_FLAGS ${CGO_LDFLAGS:-}"
+export CC=aarch64-linux-android21-clang
+export CXX=aarch64-linux-android21-clang++
+export CGO_ENABLED="1"
+export GOOS="android"
+export GOARCH="arm64"
+echo "[+] Building .so library"
+/usr/local/go/bin/go build -tags linux -ldflags="-buildid=" -v -trimpath -buildvcs=false -o liboutline.so -buildmode c-shared ./kotlin_exports/
+echo "[+] Move .so library"
+mkdir -p ../kmp_module/outline/src/main/cpp/include/arm64-v8a/
+mkdir -p ../kmp_module/outline/src/main/jniLibs/arm64-v8a
+cp liboutline.so ../kmp_module/outline/src/main/jniLibs/arm64-v8a/liboutline.so
+cp liboutline.h ../kmp_module/outline/src/main/cpp/include/arm64-v8a/liboutline.h
--- swift_module/CommonDI/NetCheckManagerImpl.swift
+++ swift_module/CommonDI/NetCheckManagerImpl.swift
@@ -2,8 +2,16 @@ import app
 import MyLibrary
 
 public class NetCheckManagerImpl: NetCheckManager {
+    private static let netCheckPath = NetCheckRepository_iosKt.provideNetCheckConfigPath()
+
     public func start() -> String {
-        return Cloak_outlineNetCheck()
+        var err: NSError?
+        Cloak_outlineNetCheck(netCheckPath, &err)
+        if let error = err {
+            return "failed to start netcheck: \(error.localizedDescription)"
+        } else {
+            return ""
+        }
     }
 
     public func cancel() {
