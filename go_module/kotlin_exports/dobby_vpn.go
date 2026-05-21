b6180f45c1d8ea2e08850584e6c5408b9ce09ba4
brudloord <brudloord@yandex.ru>
2026-05-21 13:28:21 +0300

    Fix outline

--- go_module/kotlin_exports/dobby_vpn.go
+++ go_module/kotlin_exports/dobby_vpn.go
@@ -1,154 +1,121 @@
-//go:build android
-
-package main
-
-/*
-#include <stdlib.h>
-#include <string.h>
-*/
-import "C"
-
 import (
-	"os"
-	"runtime/debug"
-	"sync"
+    "fmt"
+    "os"
+    "runtime/debug"
+    "strings"
+    "sync"
 
-	"go_module/core"
-	"go_module/core/pkg"
-	"go_module/log"
-	"go_module/outline"
-	"go_module/xray"
+    "go_module/core"
+    "go_module/core/pkg"
+    "go_module/log"
+    "go_module/outline"
+    "go_module/xray"
 )
 
 var vpnClient *core.CoreClient
 var lastError string
 var errorMu sync.Mutex
-
-// ==========================================
-// ERROR HANDLING & GUARDS
-// ==========================================
-
-//export GetVpnLastError
-func GetVpnLastError() *C.char {
-	errorMu.Lock()
-	defer errorMu.Unlock()
-	if lastError == "" {
-		return nil
-	}
-	return C.CString(lastError)
-}
-
-func clearLastError() {
-	errorMu.Lock()
-	defer errorMu.Unlock()
-	lastError = ""
-}
-
-func setLastError(err string) {
-	errorMu.Lock()
-	defer errorMu.Unlock()
-	lastError = err
-	log.Infof("Error set: %s", err)
-}
-
-func guardExport(fnName string) func() {
-	return func() {
-		if r := recover(); r != nil {
-			msg := "panic in " + fnName + ": " + unsafeToString(r)
-			setLastError(msg)
-			log.Infof("%s\n%s", msg, string(debug.Stack()))
-		}
-	}
-}
+var clientMu sync.Mutex
 
 func unsafeToString(v any) string {
-	switch t := v.(type) {
-	case string:
-		return t
-	default:
-		return "non-string panic"
-	}
+    return fmt.Sprint(v)
 }
 
-// ==========================================
-// VPN LIFECYCLE CONTROLS
-// ==========================================
+func disconnectLocked() {
+    if vpnClient == nil {
+        log.Infof("disconnectLocked(): client is already nil")
+        return
+    }
+
+    _ = vpnClient.Disconnect()
+    vpnClient = nil
+    log.Infof("disconnectLocked(): finished")
+}
 
 //export NewVpnClient
 func NewVpnClient(config string, protocol string, fd int32) {
-	defer guardExport("NewVpnClient")()
-	log.Infof("NewVpnClient() called")
+    defer guardExport("NewVpnClient")()
 
-	// Ensure any zombie connection is completely cleaned up
-	VpnDisconnect()
+    clientMu.Lock()
+    defer clientMu.Unlock()
 
-	log.Infof("Config length=%d, protocol=%s", config, protocol)
+    log.Infof("NewVpnClient() called")
+    clearLastError()
 
-	tunFile := os.NewFile(uintptr(fd), "tun")
+    config = strings.Clone(config)
+    protocol = strings.Clone(protocol)
 
-	var device pkg.ProtocolDevice
-	var err error
+    disconnectLocked()
 
-	// Factory: Create the protocol-specific device
-	switch protocol {
-	case "xray":
-		device, err = xray.NewXrayDevice(config)
-	case "outline":
-		device, err = outline.NewOutlineDevice(config)
-	default:
-		setLastError("unsupported protocol: " + protocol)
-		log.Infof("NewVpnClient() failed: unsupported protocol")
-		return
-	}
+    log.Infof("NewVpnClient(): config.len=%d protocol=%s fd=%d", len(config), protocol, fd)
 
-	if err != nil {
-		setLastError(err.Error())
-		log.Infof("NewVpnClient() failed to create %s device: %v", protocol, err)
-		return
-	}
+    tunFile := os.NewFile(uintptr(fd), "tun")
+    if tunFile == nil {
+        setLastError("failed to create tun file from fd")
+        return
+    }
 
-	// Inject the protocol device into the universal mobile client
-	vpnClient = core.NewClient(device, tunFile)
+    var device pkg.ProtocolDevice
+    var err error
 
-	log.Infof("NewVpnClient() finished successfully")
+    switch protocol {
+    case "xray":
+        device, err = xray.NewXrayDevice(config)
+    case "outline":
+        device, err = outline.NewOutlineDevice(config)
+    default:
+        setLastError("unsupported protocol: " + protocol)
+        log.Infof("NewVpnClient() failed: unsupported protocol=%s", protocol)
+        return
+    }
+
+    if err != nil {
+        setLastError(err.Error())
+        log.Infof("NewVpnClient() failed to create %s device: %v", protocol, err)
+        return
+    }
+
+    log.Infof("NewVpnClient(): created device type=%T", device)
+
+    vpnClient = core.NewClient(device, tunFile)
+
+    log.Infof("NewVpnClient() finished successfully")
 }
 
 //export VpnConnect
 func VpnConnect() int32 {
-	defer guardExport("VpnConnect")()
-	log.Infof("VpnConnect() called")
+    defer guardExport("VpnConnect")()
 
-	clearLastError()
+    clientMu.Lock()
+    defer clientMu.Unlock()
 
-	if vpnClient == nil {
-		setLastError("client is nil")
-		log.Infof("VpnConnect() failed: client is nil")
-		return -1
-	}
+    log.Infof("VpnConnect() called")
+    clearLastError()
 
-	err := vpnClient.Connect()
-	if err != nil {
-		setLastError(err.Error())
-		log.Infof("VpnConnect() failed: %v", err)
-		return -1
-	}
+    if vpnClient == nil {
+        setLastError("client is nil")
+        log.Infof("VpnConnect() failed: client is nil")
+        return -1
+    }
 
-	log.Infof("VpnConnect() finished successfully")
-	return 0
+    if err := vpnClient.Connect(); err != nil {
+        setLastError(err.Error())
+        log.Infof("VpnConnect() failed: %v", err)
+        return -1
+    }
+
+    log.Infof("VpnConnect() finished successfully")
+    return 0
 }
 
 //export VpnDisconnect
 func VpnDisconnect() {
-	defer guardExport("VpnDisconnect")()
-	log.Infof("VpnDisconnect() called")
+    defer guardExport("VpnDisconnect")()
 
-	if vpnClient == nil {
-		log.Infof("VpnDisconnect(): client is already nil")
-		return
-	}
+    clientMu.Lock()
+    defer clientMu.Unlock()
 
-	_ = vpnClient.Disconnect()
-
-	vpnClient = nil
-	log.Infof("VpnDisconnect() finished")
+    log.Infof("VpnDisconnect() called")
+    disconnectLocked()
+    log.Infof("VpnDisconnect() finished")
 }
